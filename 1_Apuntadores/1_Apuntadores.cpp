fdd7481dcdc12fb4178228f310f5869fac8e8fb9
fmandujano <f.mandujano@saei.mx>
2023-07-24 14:27:06 -0600

    Selector con apuntadores de función

--- 1_Apuntadores/1_Apuntadores.cpp
+++ 1_Apuntadores/1_Apuntadores.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-int main( int argc, char** argv )
+void PruebaArgumentos(int argc, char** argv)
 {
 	//si el programa fue lanzado con argumentos..
 	if (argc > 1)
@@ -14,7 +14,7 @@ int main( int argc, char** argv )
 			//toda acadena de texto termina con un /0
 			int j = 0;
 			int value = 0;
-			std::cout << " "; 
+			std::cout << " ";
 
 			// argv[0][0]  
 
@@ -24,7 +24,7 @@ int main( int argc, char** argv )
 				value = (int)argv[i][j];
 				std::cout << argv[i][j] << " = " << value << " 0x" << std::hex << value << "\n ";
 				j++;
-			} while (argv[i][j] != 0 );	
+			} while (argv[i][j] != 0);
 
 
 			std::cout << "\n";
@@ -35,27 +35,30 @@ int main( int argc, char** argv )
 	{
 		std::cout << "No se dieron argumentos. Cerrando\n";
 	}
+}
 
+void Promedio(int argc, char** argv)
+{
 	//ejercicio 2 de apuntadores
-	//forma C de asignar memoria 
-	//int* x = (int*) malloc(sizeof(int));
-	// forma C++ de asignar memoria
+//forma C de asignar memoria 
+//int* x = (int*) malloc(sizeof(int));
+// forma C++ de asignar memoria
 	int* x = new int;
 
 	*x = 99;
 	std::cout << "sizeof int " << sizeof(int) << "\n";
-	std::cout << " x :" << *x << " en " << x  << "\n";
+	std::cout << " x :" << *x << " en " << x << "\n";
 
 	*x = 'k';
 	std::cout << "sizeof int " << sizeof(int) << "\n";
 	std::cout << " x :" << *x << " en " << x << "\n";
-	
+
 	*(x + 1) = 18;
-	std::cout << " x+1 :" << *(x+1) << " en " << x+1 << "\n";
+	std::cout << " x+1 :" << *(x + 1) << " en " << x + 1 << "\n";
 
 	//arreglo de 100 enteros
 	int* y = new int[100];
-	memset(y, 0, 100*sizeof(int));
+	memset(y, 0, 100 * sizeof(int));
 
 	//generar una lista de 100 enteros aleatorios
 	int* acumulador = new int;
@@ -63,10 +66,37 @@ int main( int argc, char** argv )
 	for (int i = 0; i < 100; i++)
 	{
 		y[i] = rand();
-		std::cout << " y[" << i << "] = " << *(y+i) << "\n";
+		std::cout << " y[" << i << "] = " << *(y + i) << "\n";
 		*acumulador += y[i];
 	}
 	float promedio = *acumulador / 100.0;
 	std::cout << " prmedio = " << promedio << "\n";
+}
+
+int main( int argc, char** argv )
+{
+	//apuntador a funcion
+	void (*proceso)(int, char**) = nullptr;
+
+	//preguntar al usuario la funcion deseada
+	std::cout << "Funciones disponibles: \n";
+	std::cout << "  1. Prueba de argumentos \n";
+	std::cout << "  2. Promedio: \n";
+	std::cout << "Ingresa el numero de la funcion que quieres \n";
+
+	int seleccion;
+	std::cin >> seleccion;
+	std::cout << seleccion << "mem: "<< &seleccion << "\n";
+	if (seleccion == 1)
+		proceso = &PruebaArgumentos;
+	else if(seleccion==2)
+		proceso = &Promedio;
+	else
+	{
+		std::cout << "Opcion no valida\n";
+		main(argc, argv);
+	}
+
+	(*proceso)(argc, argv);
 	return 0;
 }
